6081c29bc09b5268217373956445b634216732da
Jean-Pierre STEIN <stein.jean-pierre@mail.be>
2023-10-08 20:59:35 +0200

    add controle places + messages tables + test report

--- test_app.py
+++ test_app.py
@@ -1,8 +1,8 @@
 import pytest
 import logging
-from check import check_mail
+from check import check_mail, check_places
 from server import loadClubs
-
+PLACES_AVAILABLE = 25
 
 clubs = loadClubs()
 logging.basicConfig(level=logging.INFO)
@@ -15,6 +15,17 @@ logging.basicConfig(level=logging.INFO)
 ])
 def test_checkmail(test_email, expected_result, expected_data):
     result, data = check_mail(test_email, clubs)
-    logging.info('This is an info message')
     assert result == expected_result
     assert data == expected_data
+
+
+@pytest.mark.parametrize("test_places, expected_result", [
+    ("", 1),
+    (0, 2),
+    (-2, 3),
+    (25, 4),
+    (13, 5)
+])
+def test_checkplaces(test_places, expected_result):
+    result = check_places(test_places, PLACES_AVAILABLE)
+    assert result == expected_result
